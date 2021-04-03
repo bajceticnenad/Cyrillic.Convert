f9fbfb7e033991211c83224a27324761f24ebe6e
Nenad Bajcetic <bajceticnenad@gmail.com>
2021-04-04 00:09:17 +0200

    v2.1.2

--- push.cmd
+++ push.cmd
@@ -1,1 +1,1 @@
-dotnet nuget push Cyrillic.Convert.2.0.3.nupkg -k oy2f7qdxwnhdzzrr45oqrb67etwqrqhu3gitkaty2t4wdq -s https://api.nuget.org/v3/index.json
+dotnet nuget push Cyrillic.Convert.2.0.3.nupkg -k keys_kreiranog_nugeta -s https://api.nuget.org/v3/index.json
