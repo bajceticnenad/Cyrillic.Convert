ec5061fc625270eb63525ab56e9fea416436e8ea
teor292 <teor292@gmail.com>
2021-04-12 10:52:43 +0300

    Update ukr

--- Cyrillic.Convert/Conversion.cs
+++ Cyrillic.Convert/Conversion.cs
@@ -2,12 +2,12 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace Cyrillic.Convert
 {
     public class Conversion
     {
-
         #region "Srpski"
         public string SerbianCyrillicToLatin(string rec)
         {
@@ -98,5 +98,125 @@ namespace Cyrillic.Convert
         }
         #endregion "Bugarski"
 
+        #region Ukrainian
+
+        public string UkrainianCyrillicToLatin(string rec)
+        {
+            var slova = new Ukrainian();
+
+            StringBuilder builder = new StringBuilder(rec.Length * 2); //approximately
+
+            var special_dict = slova.getSpecialDictionary();
+
+            foreach (var pair in special_dict)
+            {
+                rec = rec.Replace(pair.Key, pair.Value);
+            }
+
+            var start_dict = slova.getStartDictionary();
+
+            var c = rec[0];
+            bool empty_prev = false;
+            if (start_dict.TryGetValue(c, out string start_str))
+            {
+                builder.Append(start_str);
+            }
+            else
+            {
+                builder.Append(c);
+                empty_prev = is_empty(c);
+            }
+
+
+            var dict = slova.getDictionary();
+
+            int length = rec.Length;
+
+            for (int i = 1; i < length; ++i)
+            {
+                c = rec[i];
+                if (empty_prev && start_dict.TryGetValue(c, out string start_tmp_str))
+                {
+                    empty_prev = false;
+                    builder.Append(start_tmp_str);
+                } 
+                else if (dict.TryGetValue(c, out string rep))
+                {
+                    empty_prev = false;
+                    builder.Append(rep);
+                } 
+                else
+                {
+                    builder.Append(c);
+                    empty_prev = is_empty(c);
+                }
+            }
+
+            return builder.ToString();
+        }
+
+        public string UkrainianLatinToCyrillic(string rec)
+        {
+            var slova = new Ukrainian();
+            var empties = empty_chars();
+
+            foreach (var pair in slova.getSpecialDictionary())
+            {
+                rec = rec.Replace(pair.Value, pair.Key);
+            }
+
+            var start_dict = slova.getStartToUkrDictionary();
+
+            //first replace start characters
+            int previous_empty_index = -1;
+            do
+            {
+                int length = rec.Length;
+                int i = previous_empty_index + 1;
+                for (; i < length; ++i)
+                {
+                    char c = rec[i];
+                    if (is_empty(c)) continue;
+
+                    foreach (var pr in start_dict)
+                    {
+                        if (rec.IndexOf(pr.Key, i, pr.Key.Length) != i) continue;
+                        rec = rec.Substring(0, i) + pr.Value + rec.Substring(i + pr.Key.Length);
+                        break;
+                    }
+                    break;
+                }
+                previous_empty_index = i;
+
+                previous_empty_index = rec.IndexOfAny(empties, previous_empty_index);
+            } while (previous_empty_index != -1);
+
+
+
+            foreach (KeyValuePair<string, string> pair in slova.getToUkrDictionary())
+            {
+                rec = rec.Replace(pair.Key, pair.Value);
+                // For upper case
+                rec = rec.Replace(pair.Key.ToUpper(), pair.Value.ToUpper());
+            }
+
+            return rec;
+
+        }
+
+        private bool is_empty(char c)
+        {
+            return c == ' '
+                        || c == '\t'
+                        || c == '\n'
+                        || c == '\r';
+        }
+
+        private char[] empty_chars()
+        {
+            return new char[] { ' ', '\t', '\n', '\r' };
+        }
+
+        #endregion
     }
 }
--- Cyrillic.Convert/Dictionaries/Ukrainian.cs
+++ Cyrillic.Convert/Dictionaries/Ukrainian.cs
@@ -6,86 +6,235 @@ namespace Cyrillic.Convert.Dictionaries
 {
     public class Ukrainian
     {
-        private Dictionary<string, string> ukr_dictionary;
-        public void RusDictionary()
+        private readonly Dictionary<char, string> ukr_to_latin_dictionary;
+        private readonly Dictionary<char, string> ukr_start_dictionary;
+        private readonly Dictionary<string, string> ukr_special_cases;
+
+        private readonly Dictionary<string, string> latin_to_ukr_dictionary;
+        private readonly Dictionary<string, string> latin_to_ukr_start_dictionary;
+
+        public Ukrainian()
         {
-            ukr_dictionary = new Dictionary<string, string>
+            ukr_start_dictionary = new Dictionary<char, string>()
             {
-                { "а", "a" },
-                { "б", "b" },
-                { "в", "v" },
-                { "г", "h" },
-                { "ґ", "g" },
-                { "д", "d" },
-                { "е", "e" },
-                { "ё", "yo" },
-                { "ж", "zh" },
-                { "з", "z" },
-                { "и", "i" },
-                { "й", "j" },
-                { "к", "k" },
-                { "л", "l" },
-                { "м", "m" },
-                { "н", "n" },
-                { "о", "o" },
-                { "п", "p" },
-                { "р", "r" },
-                { "с", "s" },
-                { "т", "t" },
-                { "у", "u" },
-                { "ф", "f" },
-                { "х", "h" },
-                { "ц", "c" },
-                { "ч", "ch" },
-                { "ш", "sh" },
-                { "щ", "sch" },
-                { "ъ", "j" },
-                { "ы", "i" },
-                { "ь", "j" },
-                { "э", "e" },
-                { "ю", "yu" },
-                { "я", "ya" },
-                { "А", "A" },
-                { "Б", "B" },
-                { "В", "V" },
-                { "Г", "H" },
-                { "Ґ", "G" },
-                { "Д", "D" },
-                { "Е", "E" },
-                { "Ё", "Yo" },
-                { "Ж", "Zh" },
-                { "З", "Z" },
-                { "И", "I" },
-                { "Й", "J" },
-                { "К", "K" },
-                { "Л", "L" },
-                { "М", "M" },
-                { "Н", "N" },
-                { "О", "O" },
-                { "П", "P" },
-                { "Р", "R" },
-                { "С", "S" },
-                { "Т", "T" },
-                { "У", "U" },
-                { "Ф", "F" },
-                { "Х", "H" },
-                { "Ц", "C" },
-                { "Ч", "Ch" },
-                { "Ш", "Sh" },
-                { "Щ", "Sch" },
-                { "Ъ", "J" },
-                { "Ы", "I" },
-                { "Ь", "J" },
-                { "Э", "E" },
-                { "Ю", "Yu" },
-                { "Я", "Ya" }
+                { 'є', "ye" },
+                { 'ї' , "yi" },
+                { 'й' , "y" },
+                { 'ю' , "yu" },
+                { 'я' , "ya" },
+
+                { 'Є', "Ye" },
+                { 'Ї' , "Yi" },
+                { 'Й' , "Y" },
+                { 'Ю' , "Yu" },
+                { 'Я' , "Ya" },
+            };
+
+            latin_to_ukr_start_dictionary = new Dictionary<string, string>()
+            {
+                { "ye", "є" },
+                { "yi", "ї" },
+                { "yu", "ю" },
+                { "ya", "я" },
+                { "y", "й" },
+
+
+                { "Ye", "Є" },
+                { "Yi", "Ї" },
+                { "Yu", "Ю" },
+                { "Ya", "Я" },
+
+                { "YE", "Є" },
+                { "YI", "Ї" },
+                { "YU", "Ю" },
+                { "YA", "Я" },
+
+                { "Y", "Й" },
+            };
+
+
+            ukr_special_cases = new Dictionary<string, string>()
+            {
+                { "зг", "zgh" },
+                { "Зг", "Zgh" },
+                { "ЗГ", "ZGH" },
+                { "зГ", "zGH" }
+            };
+
+            ukr_to_latin_dictionary = new Dictionary<char, string>
+            {
+                { 'а', "a" },
+                { 'б', "b" },
+                { 'в', "v" },
+                { 'г', "h" },
+                { 'ґ', "g" },
+                { 'д', "d" },
+                { 'е', "e" },
+                { 'є', "ie" },
+                { 'ж', "zh" },
+                { 'з', "z" },
+                { 'и', "y" },
+                { 'і', "i" },
+                { 'ї', "i" },
+                { 'й', "i" },
+                { 'к', "k" },
+                { 'л', "l" },
+                { 'м', "m" },
+                { 'н', "n" },
+                { 'о', "o" },
+                { 'п', "p" },
+                { 'р', "r" },
+                { 'с', "s" },
+                { 'т', "t" },
+                { 'у', "u" },
+                { 'ф', "f" },
+                { 'х', "kh" },
+                { 'ц', "ts" },
+                { 'ч', "ch" },
+                { 'ш', "sh" },
+                { 'щ', "shch" },
+                { 'ю', "iu" },
+                { 'я', "ia" },
+
+                { 'А', "A" },
+                { 'Б', "B" },
+                { 'В', "V" },
+                { 'Г', "H" },
+                { 'Ґ', "G" },
+                { 'Д', "D" },
+                { 'Е', "E" },
+                { 'Є', "IE" },
+                { 'Ж', "Zh" },
+                { 'З', "Z" },
+                { 'И', "Y" },
+                { 'І', "I" },
+                { 'Ї', "I" },
+                { 'Й', "i" },
+                { 'К', "K" },
+                { 'Л', "L" },
+                { 'М', "M" },
+                { 'Н', "N" },
+                { 'О', "O" },
+                { 'П', "P" },
+                { 'Р', "R" },
+                { 'С', "S" },
+                { 'Т', "T" },
+                { 'У', "U" },
+                { 'Ф', "F" },
+                { 'Х', "Kh" },
+                { 'Ц', "Ts" },
+                { 'Ч', "Ch" },
+                { 'Ш', "Sh" },
+                { 'Щ', "Shch" },
+                { 'Ю', "IU" },
+                { 'Я', "IA" },
+
+                { 'ь', "" },
+                { '\'',"" }
+            };
+
+            latin_to_ukr_dictionary = new Dictionary<string, string>
+            {
+                { "shch", "щ" },
+                { "ie", "є" },
+                { "zh", "ж" },
+                { "kh", "х" },
+                { "ts", "ц" },
+                { "ch", "ч" },
+                { "sh", "ш" },
+                { "iu", "ю" },
+                { "ia", "я" },
+
+                { "Shch" , "Щ" },
+                { "SHCH" , "Щ" },
+                { "IE", "Є" },
+                { "ZH", "Ж" },
+                { "Zh", "Ж" },
+                { "KH" , "Х" },
+                { "Kh" , "Х" },
+                { "TS" , "Ц" },
+                { "Ts" , "Ц" },
+                { "CH" , "Ч" },
+                { "Ch" , "Ч" },
+                { "SH" , "Ш" },
+                { "Sh" , "Ш" },
+                { "IU" , "Ю" },
+                { "IA" , "Я" },
+
+                { "a", "а" },
+                { "b", "б" },
+                { "v", "в" },
+                { "h", "г" },
+                { "g", "ґ" },
+                { "d", "д" },
+                { "e", "е" },
+
+                { "z",  "з" },
+                { "y", "и" },
+                { "i", "і" },
+                { "k", "к" },
+                { "l", "л" },
+                { "m", "м" },
+                { "n", "н" },
+                { "o", "о" },
+                { "p", "п" },
+                { "r", "р" },
+                { "s", "с" },
+                { "t", "т" },
+                { "u", "у" },
+                { "f", "ф" },
+
+
+
+                { "A", "А" },
+                { "B", "Б" },
+                { "V", "В" },
+                { "H", "Г" },
+                { "G", "Ґ" },
+                { "D", "Д" },
+                { "E", "Е" },
+
+                { "Z", "З" },
+                { "Y" , "И" },
+                { "I" , "І" },
+                { "K" , "К" },
+                { "L" , "Л" },
+                { "M" , "М" },
+                { "N" , "Н" },
+                { "O" , "О" },
+                { "P" , "П" },
+                { "R" , "Р" },
+                { "S" , "С" },
+                { "T" , "Т" },
+                { "U" , "У" },
+                { "F" , "Ф" },
+
             };
         }
 
-        public Dictionary<string, string> getDictionary()
+
+        public Dictionary<char, string> getDictionary()
         {
-            RusDictionary();
-            return this.ukr_dictionary;
+            return this.ukr_to_latin_dictionary;
+        }
+        public Dictionary<char, string> getStartDictionary()
+        {
+            return this.ukr_start_dictionary;
+        }
+
+        public Dictionary<string, string> getStartToUkrDictionary()
+        {
+            return this.latin_to_ukr_start_dictionary;
+        }
+
+        public Dictionary<string, string> getToUkrDictionary()
+        {
+            return this.latin_to_ukr_dictionary;
+        }
+
+        public Dictionary<string, string> getSpecialDictionary()
+        {
+            return this.ukr_special_cases;
         }
     }
 }
